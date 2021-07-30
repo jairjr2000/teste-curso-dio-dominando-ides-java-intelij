a931528528408e69d3efd3bd6a3558ed07c0a6d5
Jair Junior <jairjuni17@gmail.com>
2021-07-30 09:47:17 -0300

    creating book class

--- src/br/com/dio/model/FirstProgram.java
+++ src/br/com/dio/model/FirstProgram.java
@@ -2,10 +2,45 @@ package br.com.dio.model;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        Cat cat = new Cat("Preto", "Felix", 12);
-        Cat cat2 = new Cat("Branco", "Garfield", 13);
+        Cat cat = new Cat("Black", "Felix", 12);
+        Cat cat2 = new Cat("White", "Garfield", 13);
         System.out.println(cat);
         System.out.println(cat2);
+
+        Book book = new Book("Clean Code", 464);
+        System.out.println(book);
+    }
+}
+class Book {
+    private String name;
+    private Integer numPages;
+
+    public Book(String name, Integer numPages) {
+        this.name = name;
+        this.numPages = numPages;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPages() {
+        return numPages;
+    }
+
+    public void setNumPages(Integer numPages) {
+        this.numPages = numPages;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", numPages=" + numPages +
+                '}';
+    }
 }
